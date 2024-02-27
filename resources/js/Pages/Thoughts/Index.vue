19d2210ae275651094fcf3193c5fec8cf4bbf895
Craig McLeod <mail@camcleod.com>
2024-02-27 10:35:24 +0000

    240227-1035:  First Working Boot

--- resources/js/Pages/Thoughts/Index.vue
+++ resources/js/Pages/Thoughts/Index.vue
@@ -10,19 +10,18 @@ const form = useForm({
 </script>
  
 <template>
-    <Head title="Thoughts" />
- 
-    <AuthenticatedLayout>
-        <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
-            <form @submit.prevent="form.post(route('thoughts.store'), { onSuccess: () => form.reset() })">
-                <textarea
-                    v-model="form.message"
-                    placeholder="What's on your mind?"
-                    class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
-                ></textarea>
-                <InputError :message="form.errors.message" class="mt-2" />
-                <PrimaryButton class="mt-4">Have a Thought</PrimaryButton>
-            </form>
-        </div>
-    </AuthenticatedLayout>
+<Head title="Thoughts" />
+
+<AuthenticatedLayout>
+    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+        <form @submit.prevent="form.post(route('thoughts.store'), { onSuccess: () => form.reset() })">
+            <textarea
+            v-model="form.message"
+            placeholder="What's on your mind?"
+            class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"></textarea>
+            <InputError :message="form.errors.message" class="mt-2" />
+            <PrimaryButton class="mt-4">Have a Thought</PrimaryButton>
+        </form>
+    </div>
+</AuthenticatedLayout>
 </template>
