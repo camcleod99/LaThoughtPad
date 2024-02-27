b6b95eae3be0b83f811c8de9677536601e62d710
Craig McLeod <mail@camcleod.com>
2024-02-27 11:42:23 +0000

    240227-1142: Create Functionality Created

--- app/Http/Controllers/ThoughtController.php
+++ app/Http/Controllers/ThoughtController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Thought;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -31,9 +32,15 @@ class ThoughtController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+      $validated = $request->validate([
+        'message' => 'required|string|max:255',
+      ]);
+    
+      $request->user()->thoughts()->create($validated);
+
+      return redirect(route('thoughts.index'));
     }
 
     /**
